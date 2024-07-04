d5cacfbb9a24b5a3c9dbf36468ab516141e27f6f
AkeemKaleeb <kaileb.j.cole@gmail.com>
2024-07-04 00:26:12 -0400

    Initial Commit
    Implementation of basic file structure and layout of what code should be going in what files in order to maintain organization. Basic implementation of the CPU and registers including flag register

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+/**************************************************************************************************
+* File Name         : main.rs
+* Description       : Contains the main entry point of the application
+**************************************************************************************************/
+
+mod cpu;
+mod ppu;
+mod memory;
+mod opcodes;
+mod io;
+mod timers;
+mod interrupts;
+mod cartridge;
+mod debugger;
+mod utils;
+
+fn main() {
+    println!("Hello, world!");
+}
